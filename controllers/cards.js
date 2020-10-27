1cffb437044582bb449571277143efbc758199b4
Tamara A <akira-49-q@yandex.ru>
2020-10-27 17:45:33 +0200

    feat: добавить добавление и удаление лайков карточки

--- controllers/cards.js
+++ controllers/cards.js
@@ -49,3 +49,33 @@ module.exports.deleteCard = async (req, res) => {
     res.status(500).send({ message: 'Ошибка на сервере' });
   }
 };
+
+/** Контролер добавления лайка карточке */
+module.exports.addLike = async (req, res) => {
+  try {
+    const card = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+      { new: true },
+    );
+    if (!card) res.status(404).send({ message: 'Такая карта не существует' });
+    res.send({ data: card });
+  } catch (error) {
+    res.status(500).send({ message: 'Ошибка на сервере' });
+  }
+};
+
+/** Контролер добавления лайка карточке */
+module.exports.deleteLike = async (req, res) => {
+  try {
+    const card = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $pull: { likes: req.user._id } }, // убрать _id из массива
+      { new: true },
+    );
+    if (!card) res.status(404).send({ message: 'Такая карта не существует' });
+    res.send({ data: card });
+  } catch (error) {
+    res.status(500).send({ message: 'Ошибка на сервере' });
+  }
+};
--- routes/cards.js
+++ routes/cards.js
@@ -5,6 +5,8 @@ const {
   deleteCard,
   createCard,
   getCard,
+  addLike,
+  deleteLike,
 } = require('../controllers/cards');
 
 /** Обработка запроса всех карточек */
@@ -18,5 +20,11 @@ router.delete('/cards/:cardId', deleteCard);
 
 /** Обработка запроса отдельной карточки */
 router.get('/cards/:cardId', getCard);
+
+/** Обработка добавления лайка карточке */
+router.put('/cards/:cardId/likes', addLike);
+
+/** Обработка удаления лайка карточке */
+router.delete('/cards/:cardId/likes', deleteLike);
 
 module.exports = router;
